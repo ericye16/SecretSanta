0d45a0ad7a21a462250362ee9825eb02a4850fa5
Eric Ye <computereric@live.ca>
2012-11-29 20:27:11 -0500

    Added key-generating and reading files.

---
+++ src/keypkg.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eric
+ * Date: 29/11/12
+ * Time: 12:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class keypkg {
+}
